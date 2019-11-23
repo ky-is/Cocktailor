cc090f178e702de7c977a782f2f6f0f1c6ced045
Kyle Coburn <kylecoburn@gmail.com>
2019-11-23 11:31:57 -0800

    🌱

---
+++ Cocktailor/Scene/SceneDelegate.swift
@@ -0,0 +1,23 @@
+import UIKit
+import SwiftUI
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+	var window: UIWindow?
+
+	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+		if let windowScene = scene as? UIWindowScene {
+			let window = UIWindow(windowScene: windowScene)
+			let context = DataModel.persistentContainer.viewContext
+			let contentView = ContentView().environment(\.managedObjectContext, context)
+			window.rootViewController = UIHostingController(rootView: contentView)
+			self.window = window
+			window.makeKeyAndVisible()
+		}
+	}
+
+	func sceneDidEnterBackground(_ scene: UIScene) {
+		DataModel.saveContext()
+	}
+
+}
