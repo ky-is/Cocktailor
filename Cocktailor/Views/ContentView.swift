35b1bc2d08cd1443f9241365fcd4d2877659c499
Kyle Coburn <kylecoburn@gmail.com>
2019-11-23 15:31:00 -0800

    🎨 add tab bar to homescreen

--- Cocktailor/Views/ContentView.swift
+++ Cocktailor/Views/ContentView.swift
@@ -2,9 +2,33 @@ import SwiftUI
 
 struct ContentView: View {
 	var body: some View {
-		NavigationView {
-			Text("")
-				.navigationBarTitle("My Bar")
+		TabView {
+			NavigationView {
+				Text("")
+					.navigationBarTitle("My Bar")
+			}
+				.tabItem {
+					Image(systemName: "squares.below.rectangle")
+					Text("My Bar")
+				}
+			NavigationView {
+				Text("")
+					.navigationBarTitle("Build")
+			}
+				.navigationViewStyle(StackNavigationViewStyle())
+				.tabItem {
+					Image(systemName: "wand.and.stars")
+					Text("Build")
+				}
+			NavigationView {
+				Text("")
+					.navigationBarTitle("Explore")
+			}
+				.navigationViewStyle(StackNavigationViewStyle())
+				.tabItem {
+					Image(systemName: "staroflife")
+					Text("Explore")
+				}
 		}
 	}
 }
