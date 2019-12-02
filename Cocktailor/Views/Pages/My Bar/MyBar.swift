61c542f270cd10febfeb47507c25c9bc13db0312
Kyle Coburn <kylecoburn@gmail.com>
2019-12-01 22:57:40 -0800

    ✨ show selected ingredients count, and reset

--- Cocktailor/Views/Pages/My Bar/MyBar.swift
+++ Cocktailor/Views/Pages/My Bar/MyBar.swift
@@ -3,18 +3,34 @@ import SwiftUI
 struct MyBar: View {
 	@FetchRequest(entity: IngredientEntry.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \IngredientEntry.id, ascending: true)]) private var ingredientEntries: FetchedResults<IngredientEntry>
 
-	let displayIngredients = IngredientData.keyValues.values.filter { $0.parent == nil }.sorted { $0.id < $1.id }
+	@Environment(\.managedObjectContext) private var managedObjectContext
+	private let displayIngredients = IngredientData.keyValues.values.filter { $0.parent == nil }.sorted { $0.id < $1.id }
 
 	var body: some View {
 		var ingredientEntriesByID = [String: IngredientEntry]()
 		for ingredientEntry in ingredientEntries {
 			ingredientEntriesByID[ingredientEntry.id] = ingredientEntry
 		}
+		let ownedEntries = ingredientEntries.filter { $0.owned }
+		let ownedCount = ownedEntries.count
 		return NavigationView {
 			List(displayIngredients) { data in
 				IngredientListEntry(data: data, entry: .constant(ingredientEntriesByID[data.id]), observededIngredients: ObservableIngredients.inactive, hasCocktail: true)
 			}
-				.navigationBarTitle("My Bar")
+				.navigationBarTitle("Ingredient".pluralize(ownedCount))
+				.navigationBarItems(trailing:
+					Group {
+						if ownedCount > 0 {
+							Button(action: {
+								self.managedObjectContext.perform {
+									ownedEntries.forEach { $0.owned = false }
+								}
+							}, label: {
+								Text("Clear owned")
+							})
+						}
+					}
+				)
 		}
 			.navigationViewStyle(StackNavigationViewStyle())
 	}
