35e8d22729ea03cc5ca242bc222c1b2dfe587da5
Kyle Coburn <kylecoburn@gmail.com>
2019-12-01 22:52:47 -0800

    ✨ show most useful next ingredients to buy

--- Cocktailor/Views/Pages/Explore/Explore.swift
+++ Cocktailor/Views/Pages/Explore/Explore.swift
@@ -19,11 +19,62 @@ struct Explore: View {
 }
 
 private struct ExploreList: View {
+	@FetchRequest(entity: IngredientEntry.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \IngredientEntry.favorite, ascending: false)], predicate: NSPredicate(format: "owned == TRUE")) private var ownedIngredientEntries: FetchedResults<IngredientEntry>
+
 	let cocktails: [CocktailData]
 
 	var body: some View {
-		List {
-			Section(header: Text("All Cocktails")) {
+		let allCocktails = CocktailData.keyValues.values
+		let ownedIDs = ownedIngredientEntries.map { $0.id }
+		let cocktailsAndMissingCounts = allCocktails
+			.map { cocktail -> (cocktail: CocktailData, missingIngredientCount: Int) in
+				let missingIngredientCount = cocktail.ingredients.reduce(0) { ownedIDs.contains($1.id) ? $0 : $0 + 1 }
+				return (cocktail, missingIngredientCount)
+			}
+			.filter { $0.missingIngredientCount > 0 }
+		let mostMissingIngredients = Double(cocktailsAndMissingCounts.reduce(1) { min($0, $1.missingIngredientCount) })
+		var missingIngredientScoresByID = [String: (count: Int, score: Double)]()
+		for cocktailAndMissingCount in cocktailsAndMissingCounts {
+			let missingIngredientCount = cocktailAndMissingCount.missingIngredientCount
+			let cocktailScore = 1 / (Double(missingIngredientCount) / mostMissingIngredients)
+			for ingredient in cocktailAndMissingCount.cocktail.ingredients {
+				let ingredientID = ingredient.id
+				guard !ownedIDs.contains(ingredientID) else {
+					continue
+				}
+				if missingIngredientScoresByID[ingredientID] != nil {
+					missingIngredientScoresByID[ingredientID]!.count += 1
+					missingIngredientScoresByID[ingredientID]!.score += cocktailScore
+				} else {
+					missingIngredientScoresByID[ingredientID] = (1, cocktailScore)
+				}
+			}
+		}
+		let bestIngredientIDs = missingIngredientScoresByID.sorted(by: { $0.value.score > $1.value.score }).prefix(3)
+		return List {
+			if !bestIngredientIDs.isEmpty {
+				Section(header: Text("Next ingredients")) {
+					ForEach(bestIngredientIDs, id: \.key) { ingredientScore in
+//						let ingredient = IngredientData.keyValues[ingredientScore.key]! //TODO
+						NavigationLink(destination: IngredientEntryDetail(data: IngredientData.keyValues[ingredientScore.key]!)) {
+							HStack {
+								IngredientListItem(data: IngredientData.keyValues[ingredientScore.key]!, hasCocktail: true)
+								Spacer()
+								HStack {
+									Text("used in ")
+										+
+										Text(ingredientScore.value.count.description)
+											.bold()
+										+
+										Text(" cocktails")
+								}
+								.font(.caption)
+							}
+						}
+					}
+				}
+			}
+			Section(header: Text("All cocktails")) {
 				BuildCocktailsDetailListEntries(cocktails: cocktails)
 			}
 		}
